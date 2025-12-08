6ebbab6a9e4aa1a43f26a7bca05ce5ce4ad3003e
Lisery <1143765064@qq.com>
2025-12-08 21:32:08 +0800

    fix: 修复mesh的显示问题，增加多个模型示例

--- case_05_load_mesh/src/main.rs
+++ case_05_load_mesh/src/main.rs
@@ -1,14 +1,15 @@
 use glotus::{
-    AntiPixel, AppConfig, Color, Entity, Material, Mesh, PointLight, Position, Shader, Transform, UniformValue
+    AntiPixel, AppConfig, Color, Entity, Material, Mesh, PointLight, Position, Shader, Transform,
+    UniformValue,
 };
 
 fn main() {
     let app = glotus::App::new_with_config(AppConfig {
-        bg_color: [0.0, 0.0, 0.0],
+        // bg_color: [0.0, 0.0, 0.0],
         anti_pixel_msaa: AntiPixel::MSAA16,
         ..Default::default()
     });
-    
+
     let shader = Shader::from_files(
         concat!(env!("CARGO_PKG_NAME"), "/assets/shaders/vs_0.vert"),
         concat!(env!("CARGO_PKG_NAME"), "/assets/shaders/fs_0.frag"),
@@ -41,13 +42,51 @@ fn main() {
         .insert_uniform("material.specular_shininess", UniformValue::Float(40.0));
 
     let mesh = Mesh::load_obj(concat!(env!("CARGO_PKG_NAME"), "/assets/meshes/sphere.obj"));
-
-    let entity = Entity::new(Transform::default(), material.clone(), mesh.clone());
+    let mesh_2 = Mesh::load_obj(concat!(
+        env!("CARGO_PKG_NAME"),
+        "/assets/meshes/sphere_no_smooth.obj"
+    ));
+    let mesh_3 = Mesh::load_obj(concat!(env!("CARGO_PKG_NAME"), "/assets/meshes/box.obj"));
+    let mesh_4 = Mesh::load_obj(concat!(
+        env!("CARGO_PKG_NAME"),
+        "/assets/meshes/suzanne.obj"
+    ));
 
     app.borrow()
         .get_world()
         .borrow_mut()
-        .add_entity(entity.clone());
+        .add_entity(Entity::new(
+            Transform::from_position(0.0, 0.0, 0.0),
+            material.clone(),
+            mesh.clone(),
+        ));
+
+    app.borrow()
+        .get_world()
+        .borrow_mut()
+        .add_entity(Entity::new(
+            Transform::from_position(3.0, 0.0, 0.0),
+            material.clone(),
+            mesh_2.clone(),
+        ));
+
+    app.borrow()
+        .get_world()
+        .borrow_mut()
+        .add_entity(Entity::new(
+            Transform::from_position(0.0, 0.0, 3.0),
+            material.clone(),
+            mesh_3.clone(),
+        ));
+
+    app.borrow()
+        .get_world()
+        .borrow_mut()
+        .add_entity(Entity::new(
+            Transform::from_position(3.0, 0.0, 3.0),
+            material.clone(),
+            mesh_4.clone(),
+        ));
 
     app.borrow()
         .get_world()
@@ -55,7 +94,7 @@ fn main() {
         .get_camera()
         .borrow_mut()
         .get_transform_mut()
-        .set_position(Position::new(0.0, 0.0, 4.0));
+        .set_position(Position::new(1.5, 0.0, 6.0));
 
     let point_light = PointLight::new();
     point_light.borrow_mut().color = Color::from_rgb(255, 255, 255);
--- glotus/src/render/mesh/mesh.rs
+++ glotus/src/render/mesh/mesh.rs
@@ -27,34 +27,55 @@ impl Mesh {
         let mesh = &models[0].mesh;
 
         let mut vertices = Vec::new();
-        for i in 0..mesh.positions.len() / 3 {
+
+        for i in 0..mesh.indices.len() {
+            let position_index = mesh.indices[i] as usize;
+
+            let position = Vector3::new(
+                mesh.positions[position_index * 3],
+                mesh.positions[position_index * 3 + 1],
+                mesh.positions[position_index * 3 + 2],
+            );
+
+            // 法线要检查是否存在
+            let normal = if !mesh.normals.is_empty() {
+                let normal_index = mesh.normal_indices[i] as usize;
+                Vector3::new(
+                    mesh.normals[normal_index * 3],
+                    mesh.normals[normal_index * 3 + 1],
+                    mesh.normals[normal_index * 3 + 2],
+                )
+            } else {
+                Vector3::new(0.0, 1.0, 0.0)
+            };
+
+            // UV 要检查 size
+            let tex_coord = if !mesh.texcoords.is_empty() {
+                let texture_index = mesh.texcoord_indices[i] as usize;
+                Vector2::new(
+                    mesh.texcoords[texture_index * 2],
+                    mesh.texcoords[texture_index * 2 + 1],
+                )
+            } else {
+                Vector2::new(0.0, 0.0)
+            };
+
             vertices.push(Vertex {
-                position: Vector3::new(
-                    mesh.positions[i * 3],
-                    mesh.positions[i * 3 + 1],
-                    mesh.positions[i * 3 + 2],
-                ),
-                normal: if !mesh.normals.is_empty() {
-                    Vector3::new(
-                        mesh.normals[i * 3],
-                        mesh.normals[i * 3 + 1],
-                        mesh.normals[i * 3 + 2],
-                    )
-                } else {
-                    Vector3::new(0.0, 0.0, 0.0)
-                },
-                tex_coord: if !mesh.texcoords.is_empty() {
-                    Vector2::new(mesh.texcoords[i * 2], mesh.texcoords[i * 2 + 1])
-                } else {
-                    Vector2::new(0.0, 0.0)
-                },
+                position,
+                normal,
+                tex_coord,
                 tangent: Vector3::new(0.0, 0.0, 0.0),
                 bitangent: Vector3::new(0.0, 0.0, 0.0),
                 color: Vector3::new(1.0, 1.0, 1.0),
             });
         }
 
-        Self::new(vertices, mesh.indices.clone())
+        Self::new(
+            vertices,
+            (0..mesh.indices.len())
+                .map(|x| x as u32)
+                .collect::<Vec<u32>>(),
+        )
     }
 
     pub fn get_vertices(&self) -> &Vec<Vertex> {
